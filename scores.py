3b117dfa542f19d8b1f15ce11732565ff6c785fc
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-03-14 13:56:22 +1000

    fix negative input

--- scores.py
+++ scores.py
@@ -3,4 +3,8 @@ score = int(input("Score: "))
 while score >= 0:
     scores.append(score)
     score = int(input("Score: "))
-print("Your highest score is {}".format(max(scores)))
+
+if scores == []:
+    print("List is empty")
+else:
+    print("Your highest score is {}".format(max(scores)))
