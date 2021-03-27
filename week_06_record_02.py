02db98a2c09e60eebff4ea9c79dffe57624b9184
LukeElliman <luke.elliman@my.jcu.edu.au>
2021-03-28 01:08:59 +1000

    Add method to give taco

--- week_06_record_02.py
+++ week_06_record_02.py
@@ -11,6 +11,9 @@ class User:
         self.number_of_tacos -= give_amount
         return self.number_of_tacos
 
+    def __add_tacos__(self):
+        self.number_of_tacos += give_amount
+        return self.number_of_tacos
 
 
 name = str(input("Name: "))
@@ -18,12 +21,21 @@ score = int(input("Score: "))
 
 user1 = User(name, score)
 
+name = str(input("Name: "))
+score = int(input("Score: "))
+
+user2 = User(name, score)
+
 
 print("{}, {} points, {} tacos left".format(user1.name, user1.score, user1.number_of_tacos))
-give_choice = str(input("Do you want to give any tacos (Y/N): "))
+print("{}, {} points, {} tacos left".format(user2.name, user2.score, user2.number_of_tacos))
+
+give_choice = str(input("Do you want to give any tacos to user 1 (Y/N): "))
 if give_choice == "Y":
     give_amount = int(input("How many? "))
-    user1.__give__()
-    print("{}, {} points, {} tacos left".format(user1.name, user1.score, user1.number_of_tacos))
+    user2.__give__()
+    user1.__add_tacos__()
+    print("User 2 {}, {} points, {} tacos left".format(user2.name, user2.score, user2.number_of_tacos))
+    print("User 1 {}, {} points, {} tacos left".format(user1.name, user1.score, user1.number_of_tacos))
 else:
     print("bye")
